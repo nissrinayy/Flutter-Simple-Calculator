db1fe5ed724b7467221ab4022885e74cc6b47ad8
nissrinayy <rescien5@gmail.com>
2026-04-15 21:23:46 +0700

    main screen update

--- flutter_calculator/lib/screen/main_screen.dart
+++ flutter_calculator/lib/screen/main_screen.dart
@@ -144,69 +144,70 @@ class MainScreen extends StatelessWidget {
   Widget outPutSection(
       ThemeController themeController, CalculateController controller) {
     return Expanded(
-        child: Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        /// theme switcher
-        Padding(
-          padding: const EdgeInsets.only(top: 40),
-          child: GetBuilder<ThemeController>(builder: (controller) {
-            return AdvancedSwitch(
-              controller: controller.switcherController,
-              activeImage: const AssetImage('assets/day_sky.png'),
-              inactiveImage: const AssetImage('assets/night_sky.jpg'),
-              activeColor: Colors.green,
-              inactiveColor: Colors.grey,
-              activeChild: Text(
-                'Day',
-                style: GoogleFonts.ubuntu(
-                    fontWeight: FontWeight.bold, fontSize: 17),
-              ),
-              inactiveChild: Text(
-                'Night',
-                style: GoogleFonts.ubuntu(
-                    fontWeight: FontWeight.bold, fontSize: 16),
-              ),
-              borderRadius: const BorderRadius.all(Radius.circular(1000)),
-              width: 100.0,
-              height: 45.0,
-              enabled: true,
-              disabledOpacity: 0.5,
-            );
-          }),
-        ),
-
-        /// Main Result - user input and output
-        Padding(
-          padding: const EdgeInsets.only(right: 20, top: 70),
+        child: SingleChildScrollView(
           child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Container(
-                alignment: Alignment.centerRight,
-                child: Text(
-                  controller.userInput,
-                  style: GoogleFonts.ubuntu(
-                      color:
-                          themeController.isDark ? Colors.white : Colors.black,
-                      fontSize: 38),
-                ),
+              /// theme switcher
+              Padding(
+                padding: const EdgeInsets.only(top: 40),
+                child: GetBuilder<ThemeController>(builder: (controller) {
+                  return AdvancedSwitch(
+                    controller: controller.switcherController,
+                    activeImage: const AssetImage('assets/day_sky.png'),
+                    inactiveImage: const AssetImage('assets/night_sky.jpg'),
+                    activeColor: Colors.green,
+                    inactiveColor: Colors.grey,
+                    activeChild: Text(
+                      'Day',
+                      style: GoogleFonts.ubuntu(
+                          fontWeight: FontWeight.bold, fontSize: 17),
+                    ),
+                    inactiveChild: Text(
+                      'Night',
+                      style: GoogleFonts.ubuntu(
+                          fontWeight: FontWeight.bold, fontSize: 16),
+                    ),
+                    width: 100.0,
+                    height: 45.0,
+                    enabled: true,
+                    disabledOpacity: 0.5,
+                  );
+                }),
               ),
-              Container(
-                alignment: Alignment.bottomRight,
-                child: Text(
-                  controller.userOutput,
-                  style: GoogleFonts.ubuntu(
-                    fontWeight: FontWeight.bold,
-                    color: themeController.isDark ? Colors.white : Colors.black,
-                    fontSize: 60,
-                  ),
+
+              /// Main Result - user input and output
+              Padding(
+                padding: const EdgeInsets.only(right: 20, top: 70),
+                child: Column(
+                  children: [
+                    Container(
+                      alignment: Alignment.centerRight,
+                      child: Text(
+                        controller.userInput,
+                        style: GoogleFonts.ubuntu(
+                            color:
+                                themeController.isDark ? Colors.white : Colors.black,
+                            fontSize: 38),
+                      ),
+                    ),
+                    Container(
+                      alignment: Alignment.bottomRight,
+                      child: Text(
+                        controller.userOutput,
+                        style: GoogleFonts.ubuntu(
+                          fontWeight: FontWeight.bold,
+                          color: themeController.isDark ? Colors.white : Colors.black,
+                          fontSize: 60,
+                        ),
+                      ),
+                    ),
+                  ],
                 ),
               ),
             ],
           ),
-        ),
-      ],
-    ));
+        ));
   }
 
   /// is Operator Check
